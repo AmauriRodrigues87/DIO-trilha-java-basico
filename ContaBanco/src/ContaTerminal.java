7e06a2cfa5a5ded7bcc178a7feb91f3c61753f2d
Amauri <amauri-rorp@bol.com.br>
2024-05-14 07:45:01 -0300

    inserido orientação

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -12,11 +12,11 @@ public class ContaTerminal {
        double saldo = 237.48;
 
        System.out.println("Por favor, digite o número da Agência !");
-       agencia = scanner.nextInt();
+       agencia = scanner.nextInt();  //Exemplo digitado pelo usuário 4563
 
        System.out.println("Por favor, digite o número da Conta !");
        scanner.nextLine();
-       conta = scanner.nextLine();
+       conta = scanner.nextLine(); //Exemplo digitado pelo usuário 123-4
 
 
         System.out.println("Olá "+nomeCliente+", obrigado por acessar sua conta em nosso banco, sua agência é "+agencia+", conta "+conta+" e seu saldo "+saldo+" já está disponível para saque");
